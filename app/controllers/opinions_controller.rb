ad18e89b3daa36c63db7800b6e13056a2cb0122b
Luqman Musah <musahluqman@gmail.com>
2021-05-17 08:43:37 +0000

    Add user model

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -26,7 +26,7 @@ class OpinionsController < ApplicationController
 
     respond_to do |format|
       if @opinion.save
-        format.html { redirect_to @opinion, notice: "Opinion was successfully created." }
+        format.html { redirect_to root_path, notice: "Opinion was successfully created." }
         format.json { render :show, status: :created, location: @opinion }
       else
         format.html { render :new, status: :unprocessable_entity }
