fbcf192ead6ad0ecd4ad2986c10780bceea44cf0
Luqman Musah <musahluqman@gmail.com>
2021-05-18 14:47:22 +0000

    Add user profile and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :opinions, foreign_key: "author_id"
+  has_many :opinions, foreign_key: "author_id", class_name: 'Opinion' 
 end
