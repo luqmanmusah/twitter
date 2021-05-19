be06ac3d06f292e3f6e55a5afefb5e1939e6707d
Luqman Musah <musahluqman@gmail.com>
2021-05-19 14:46:40 +0000

    Add friendship controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,30 @@ class User < ApplicationRecord
   has_many :opinions, foreign_key: "author_id", class_name: 'Opinion' 
   has_many :friendships, foreign_key: "followed_id", class_name: 'friendship'
   has_many :inverse_friendships, foreign_key: "follower_id", class_name: 'friendship'
+end
 
+def followers
+  followers_array = friendships.map{|friendship| friendship.follower if friendship.confirmed}
+  followers_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+  followers_array.compact
+end
 
+# Users who have yet to confirm follower requests
+def pending_followers
+  friendships.map{|friendship| friendship.follower if !friendship.confirmed}.compact
+end
+
+# Users who have requested to be followers
+def follower_requests
+  inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+end
+
+def confirm_follower(user)
+  friendship = inverse_friendships.find{|friendship| friendship.user == user}
+  friendship.confirmed = true
+  friendship.save
+end
+
+def follower?(user)
+  followers.include?(user)
 end
