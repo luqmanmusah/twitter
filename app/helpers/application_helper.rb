ee410b9c2108a7c2881d37aec20c23e0d6ccb8af
Luqman Musah <musahluqman@gmail.com>
2021-06-01 15:21:19 +0000

    Add N+1 queries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-  # Users who are yet to be followed
-  def pending_followers
-    partners = [].concat(current_user.followers, current_user.followed_users).map(&:id)
-    User.all.reject { |user| current_user.id == user.id || user.id.in?(partners) }
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,3 +19,9 @@ class User < ApplicationRecord
 
   scope :most_recent, -> { order(created_at: :desc) }
 end
+
+  # Users who are yet to be followed
+  def pending_followers
+    partners = [].concat(current_user.followers, current_user.followed_users).map(&:id)
+    User.all.reject { |user| current_user.id == user.id || user.id.in?(partners) }
+  end
