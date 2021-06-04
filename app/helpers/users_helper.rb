b9bfc3835fe76d04f439909db76e20bcaf8bbe0c
Luqman Musah <musahluqman@gmail.com>
2021-06-04 15:43:42 +0000

    Fix linter errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,2 @@
 module UsersHelper
-    def profile_photo(user)
-        if @user.photo.attached? 
-            img src="<%= url_for(@user.photo)
-         else 
-         
-        gravatar_image_tag(current_user.email, size: 64, alt: current_user.username)
-        end
-
-        
-    end
 end
